b0f2087d77fcf4d6475880a02769dfff5fb56146
abhishek mordani <abhishekmordani@abhisheks-MacBook-Pro.local>
2018-09-27 19:36:08 +0530

    change url

--- testindex.js
+++ testindex.js
@@ -17,7 +17,7 @@ var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
 
 
 
-driver.get('http://b3decbba.ngrok.io');
+driver.get(' http://9aaad92d.ngrok.io');
 
 driver.findElement({xpath: '//body/p'}).getText().then( function(ans){
 
